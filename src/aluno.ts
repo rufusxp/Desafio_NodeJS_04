73b079b483d02dd445e22ed9500c85c29eb0bbe0
JhonRufus <rufusxp3@gmail.com>
2022-02-15 12:13:29 -0300

    Segue desafio 4 do RUFUS

---
+++ src/aluno.ts
@@ -0,0 +1,13 @@
+class Aluno {
+    nome: string;
+    idade: number;
+    nota: number;
+
+
+    constructor(nome: string, idade: number, nota: number,) {
+        this.nome = nome;
+        this.idade = idade;
+        this.nota = nota;
+    }
+}
+module.exports = Aluno
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { Console } from "console";
+
+
+let Aluno = require('./aluno.ts');
 
 dotenv.config();
 
@@ -33,11 +37,23 @@ app.use(express.json());
  * Server Activation
  */
 
+
+
+
+
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
+	let aluno1 = new Aluno("Rufus", 30, 8.5);
+	let aluno2 = new Aluno("Cachorro", 25, 10);
+	let aluno3 = new Aluno("Gato", 15, 9);
 
+	let alunos: Array<Aluno> = [];
+	alunos.push(aluno1, aluno2, aluno3);
 
+	let somaNota = 0;
+	alunos.forEach(i => somaNota += i.nota);
+	console.log(`A Soma todas Notas é : ${somaNota}`);
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
-	
+
 });
